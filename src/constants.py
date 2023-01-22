ac5f1a1b60624d65b446e6b9e4363c627cf58a40
BrynM <badmonkey0001@gmail.com>
2023-01-21 20:53:51 -0800

    cut v1.0.1

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ import re
 
 # Used for padding large numbers in sorted strings
 ARBITRARY_NUMBER_SIZE = 16
-THIS_VERSION='v1.0.0'
+THIS_VERSION='v1.0.1'
 BASE_COMPUTER_OBJECTID = '^BASE_FLAG'
 DATETIME_FORMAT_LOCAL = '%a %Y-%m-%d %H:%M:%S'
 DATETIME_FORMAT_UTC = '%a %Y-%m-%d %H:%M:%S UTC'
